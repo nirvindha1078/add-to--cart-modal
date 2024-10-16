00e7bf8acb25289b87aff89486bc541da7f4f8cd
Nirvindha B <nirvindha2003@gmail.com>
2024-10-16 16:29:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import CartModal from './components/CartModal';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    if (cart.some(item => item.id === product.id)) {
+      alert('Item already added to the cart');
+    } else {
+      setCart([...cart, product]);
+    }
+  };
+
+  const handleRemoveFromCart = (productId) => {
+    setCart(cart.filter(item => item.id !== productId));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar cartCount={cart.length} setIsModalOpen={setIsModalOpen} />
+      <ProductList products={products} handleAddToCart={handleAddToCart} />
+      <CartModal 
+        isOpen={isModalOpen} 
+        setIsModalOpen={setIsModalOpen} 
+        cart={cart} 
+        handleRemoveFromCart={handleRemoveFromCart} 
+      />
     </div>
   );
-}
+};
 
 export default App;
